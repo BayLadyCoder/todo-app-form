509b55716eb2fb20ab6e01236789b0b96e120a2e
BayLadyCoder <rachada_32@hotmail.com>
2019-08-12 02:37:18 -0400

    Updates

---
+++ precache-manifest.08feea1916a10184acdcda9f377826cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a418a659d9861503d674de05a8ab00ed",
+    "url": "/todo-app-form/index.html"
+  },
+  {
+    "revision": "07694f7fb90149c3e529",
+    "url": "/todo-app-form/static/css/main.bddd7466.chunk.css"
+  },
+  {
+    "revision": "b63a926e82fde614b7fa",
+    "url": "/todo-app-form/static/js/2.87c42810.chunk.js"
+  },
+  {
+    "revision": "07694f7fb90149c3e529",
+    "url": "/todo-app-form/static/js/main.db777ed7.chunk.js"
+  },
+  {
+    "revision": "e7853c3fdf07b68f57b1",
+    "url": "/todo-app-form/static/js/runtime~main.d7b4f23e.js"
+  }
+]);
